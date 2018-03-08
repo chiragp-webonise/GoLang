faa1bd103370488b828b053708bd1a445eee1a7c
webonise <chirag.painter@weboniselab.com>
2018-03-08 15:11:13 +0530

    excercise1 file added with part 1

---
+++ excercise1.go
@@ -0,0 +1,60 @@
+package main 
+
+import (
+		"encoding/csv"
+		"fmt"
+		"flag"
+		"os"
+		"strings"
+	)
+
+func main(){
+
+	csvFile:= flag.String("csv","file.csv","comma seperated file")
+
+	flag.Parse()
+
+	file,err :=os.Open(*csvFile)
+
+	
+	if err != nil{
+		exit(fmt.Sprintf("Failed to open the CSV file:%s\n",*csvFile))
+	}
+
+	r:= csv.NewReader(file)
+
+	lines, err:= r.ReadAll()
+	if err!= nil {
+		exit("Failed to parse the provided csv file")
+	}
+	problems:=parseLines(lines)
+	fmt.Println(problems)
+	correct:=0
+	for i, p:= range problems{
+		fmt.Printf("Problem #%d: %s = \n",i+1,p.q)
+		var answer string
+		fmt.Scanf("%s\n",&answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+	fmt.Printf("You scored %d out of %d.\n",correct,len(problems))
+}
+func parseLines(lines [][]string) []problem{
+	ret := make([]problem,len(lines))
+	for i,line := range lines{
+		ret[i] = problem {
+			q: line[0],
+			a:strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+type problem struct {
+	q string
+	a string
+}
+func exit(msg string) {
+		fmt.Println(msg)
+		os.Exit(1)
+	}
