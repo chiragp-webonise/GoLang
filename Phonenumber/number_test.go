2c0ea2bb23162d77613e423d9a0e550abe32a395
webonise <chirag.painter@weboniselab.com>
2019-06-13 16:54:08 +0530

    webhook testing

--- Phonenumber/number_test.go
+++ Phonenumber/number_test.go
@@ -29,7 +29,7 @@ func TestNumber(t *testing.T) {
 }
 func BenchmarkNumber(b *testing.B) {
 	//comment for webhooktest
-	for j := 0; j < b.N; j++ {
+	for j := 0; j < b.T; j++ {
 		//comment for webhooktest
 		for _, test := range numberTests {
 			Number(test.input)
